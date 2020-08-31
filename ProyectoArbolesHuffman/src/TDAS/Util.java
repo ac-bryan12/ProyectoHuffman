470040f9fb76495fde50d5765d9e3ceb4349666b
Bryan Alava <bpalava@espol.edu.ec>
2020-08-31 17:42:50 -0500

    Modicaciones clase Util

--- ProyectoArbolesHuffman/src/TDAS/Util.java
+++ ProyectoArbolesHuffman/src/TDAS/Util.java
@@ -34,32 +34,61 @@ public class Util {
         return line;
     }
     public static HashMap<String,Integer> calcularFrecuencias(String texto){
-        HashMap<String,Integer> mapa = new HashMap<String,Integer>();
+        HashMap<String,Integer> mapa = new HashMap<>();
         String[] caracteres = texto.split("");
-        for(int i=0;i<caracteres.length;i++){
-            if(mapa.containsKey(caracteres[i])){
-                mapa.put(caracteres[i],mapa.get(caracteres[i])+1);
-            }else{
-                mapa.put(caracteres[i],1);
+        for (String caracter : caracteres) {
+            if (mapa.containsKey(caracter)) {
+                mapa.put(caracter, mapa.get(caracter) + 1);
+            } else {
+                mapa.put(caracter, 1);
             }  
         }
         return mapa;
     }
     public static String binarioHexadecimal(String binario){
-        //habria que ver que se utiliza si una lista o algo mas
         String hexadecimal = "";
-        String[] bin = binario.split("");
-        for(int i = 0;i< bin.length;i+=4){ //Verificar condiciones acerca de que pasa cuando solo hay un numero binario o varios pero no 4
-            int e = Integer.parseInt(bin[i]+bin[i+1]+bin[i+2]+bin[i+3],2);
-            hexadecimal += Integer.toHexString(e);
+        String[] bin = binario.split(" ");
+        for(int i = 0;i< bin.length;i+=1){ 
+            hexadecimal+=validacionBinario(bin[i]);
         }
         return hexadecimal;
     }
-
-    public String hexadecimalBinario(String hexadecimal){
-        
-        return "";
+    private static String validacionBinario(String cadena){
+        switch (cadena.length()) {
+            case 4:
+                return String.valueOf(Integer.toHexString(Integer.parseInt(cadena,2)));
+            case 3:
+                return String.valueOf(Integer.toHexString(Integer.parseInt(cadena,2)))+"-";
+            case 2:
+                return String.valueOf(Integer.toHexString(Integer.parseInt(cadena,2)))+"--";
+            case 1:
+                return String.valueOf(Integer.toHexString(Integer.parseInt(cadena,2)))+"---";
+        }  
+        return null;   
     }
+    public static String hexadecimalBinario(String hexadecimal){
+        String binario = "";
+        String[] bin = hexadecimal.split(" ");
+        for(int i = 0;i< bin.length;i+=1){ 
+            hexadecimal+=validacionHexadecimal(bin[i]);
+        }
+        return hexadecimal;
+    }
+    //Validacion inversa
+    private static String validacionHexadecimal(String cadena){
+        switch (cadena.length()) {
+            case 4:
+                return String.valueOf(Integer.parseInt(cadena,16));
+            case 3:
+                return String.valueOf(Integer.parseInt(cadena,16))+"-";
+            case 2:
+                return String.valueOf(Integer.parseInt(cadena,16))+"--";
+            case 1:
+                return String.valueOf(Integer.parseInt(cadena,16))+"---";
+        }  
+        return null;   
+    }
+
     
     public void guardarTexto(String nombreArchivo, String texto, HashMap<String,String> mapa){
         
